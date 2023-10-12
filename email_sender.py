7f7d6481bdff8248d2922aeb30fd30031fb89d48
Hamster-Inside <arkadiusz.budkowski.dev@gmail.com>
2023-10-12 17:19:05 +0200

    Changed email_sender into Context Manager so it can be used with 'with'

--- email_sender.py
+++ email_sender.py
@@ -1,32 +1,43 @@
 import smtplib, ssl, keyring
 from email.message import EmailMessage
-from os import getenv
-from dotenv import load_dotenv
 
 
 class EmailSender:
-    def __init__(self):
-        load_dotenv()
-        self.sender = getenv('EMAIL')
+    def __init__(self, port, smtp_server, credentials, ssl_enabled=False):
+        self.port = port
+        self.smtp_server = smtp_server
+        self.credentials = credentials
+        self.ssl_enabled = ssl_enabled
+
         self.msg = EmailMessage()
-        self.msg['From'] = self.sender
-        self.password = keyring.get_password("gmail", self.sender)
+        self.msg['From'] = self.credentials.username
+        self.password = keyring.get_password("gmail", self.credentials.username)
         if self.password is None:
             # password not saved in keyring
-            self.password = input("Input your password for Gmail: ")
-            keyring.set_password("gmail", self.sender, self.password)
+            # For manual input uncomment 2 lines below
+            # self.password = input("Input your password for Gmail: ")
+            # keyring.set_password("gmail", self.sender, self.password)
+
+            # password from .env file
+            keyring.set_password("gmail", self.credentials.username, self.credentials.password)
+
+    def __enter__(self):
+        if not self.ssl_enabled:
+            self.connection = smtplib.SMTP(self.smtp_server, self.port)
+        else:
+            context = ssl.create_default_context()
+            self.connection = smtplib.SMTP_SSL(self.smtp_server, self.port, context=context)
+
+        self.connection.login(self.credentials.username, self.credentials.password)
+
+        return self
 
     def send_mail(self, recipient, subject, message_content):
         self.msg['To'] = recipient
         self.msg.set_content(message_content)
         self.msg['Subject'] = subject
 
-        smtp_server = "smtp.gmail.com"
-        port = 465
-        context = ssl.create_default_context()
+        self.connection.send_message(self.msg)
 
-        with smtplib.SMTP_SSL(smtp_server, port=port, context=context) as server:
-            server.login(self.sender, self.password)
-            server.send_message(self.msg)
-            # server.sendmail(sender_email, "arkadiusz.budkowski.dev@gmail.com", 'wow is wow as wonsz')
-            server.close()
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.close()
